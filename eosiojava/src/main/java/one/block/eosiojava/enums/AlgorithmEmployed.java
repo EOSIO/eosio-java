2ebf1c3488f0ffe6c5db19acbf1b1aaccee5c2ee
anguyen <an.nguyen@block.one>
2019-04-08 17:22:42 -0400

    Added missing javadoc

--- eosiojava/src/main/java/one/block/eosiojava/enums/AlgorithmEmployed.java
+++ eosiojava/src/main/java/one/block/eosiojava/enums/AlgorithmEmployed.java
@@ -1,19 +1,31 @@
-
-
 package one.block.eosiojava.enums;
 
+/**
+ * Enum of supported algorithms which are employed in eosio-java library
+ */
 public enum AlgorithmEmployed {
+    /**
+     * Supported SECP256r1 (prime256v1) algorithm curve
+     */
     SECP256R1("secp256r1"),
+
+    /**
+     * Supported SECP256k1 algorithm curve
+     */
     SECP256K1("secp256k1"),
+
+    /**
+     * Supported prime256v1 algorithm curve
+     */
     PRIME256V1("prime256v1");
 
     private String str;
 
-    AlgorithmEmployed(String str){
+    AlgorithmEmployed(String str) {
         this.str = str;
     }
 
-    public String getString(){
+    public String getString() {
         return str;
     }
 }
