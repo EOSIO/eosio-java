6aa9ceebb4d84a7bb314c3827a09f6b54a236571
Shawn Edge <shawn.edge@block.one>
2019-04-24 13:34:46 -0400

    setup Artifactory

---
+++ .scripts/artifactory_properties.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+function copyEnvVarsToGradleProperties {
+    GRADLE_PROPERTIES=$HOME"/.gradle/gradle.properties"
+    export GRADLE_PROPERTIES
+    echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+
+    if [ ! -f "$GRADLE_PROPERTIES" ]; then
+        echo "Gradle Properties does not exist"
+
+        echo "Creating Gradle Properties file..."
+        touch $GRADLE_PROPERTIES
+
+        echo "Writing TEST_API_KEY to gradle.properties..."
+        echo "artifactory_username=(cat $BUILDKITE_HOME/.artifactory-username)" >> $GRADLE_PROPERTIES
+        echo "artifactory_password=(cat $BUILDKITE_HOME/.artifactory-password)" >> $GRADLE_PROPERTIES
+        echo "artifactory_contextURL=https://blockone.jfrog.io/blockone" >> $GRADLE_PROPERTIES
+        echo "artifactory_repo=android-libs-dev" >> $GRADLE_PROPERTIES
+    fi
+}
