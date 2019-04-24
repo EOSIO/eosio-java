5a10736c8c20604619d6c5ae5cb020de7e5e7dec
Shawn Edge <shawn.edge@block.one>
2019-04-24 14:12:51 -0400

    change url format

--- .scripts/artifactory_properties.sh
+++ .scripts/artifactory_properties.sh
@@ -12,10 +12,10 @@ function copyEnvVarsToGradleProperties {
         echo "Creating Gradle Properties file..."
         touch $GRADLE_PROPERTIES
 
-        echo "Writing TEST_API_KEY to gradle.properties..."
+        echo "Writing Secrets to gradle.properties..."
         echo "artifactory_username=(cat $BUILDKITE_HOME/.artifactory-username)" >> $GRADLE_PROPERTIES
         echo "artifactory_password=(cat $BUILDKITE_HOME/.artifactory-password)" >> $GRADLE_PROPERTIES
-        echo "artifactory_contextURL=https://blockone.jfrog.io/blockone" >> $GRADLE_PROPERTIES
+        echo "artifactory_contextURL=(https://blockone.jfrog.io/blockone)" >> $GRADLE_PROPERTIES
         echo "artifactory_repo=android-libs-dev" >> $GRADLE_PROPERTIES
     fi
 }
