47b67a277d2683368009f694828c019a4b644239
Shawn Edge <shawn.edge@block.one>
2019-04-24 14:24:51 -0400

    update script

--- .scripts/artifactory_properties.sh
+++ .scripts/artifactory_properties.sh
@@ -3,12 +3,10 @@ set -eu -o pipefail
 
 artifactory_contextURL="https://blockone.jfrog.io/blockone"
 
-function copyEnvVarsToGradleProperties {
     GRADLE_PROPERTIES="~/EOSIO/eosio-java/eosiojava/gradle.properties"
     export GRADLE_PROPERTIES
+    
     echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
-
-    if [ ! -f "$GRADLE_PROPERTIES" ]; then
         echo "Gradle Properties does not exist"
 
         echo "Creating Gradle Properties file..."
@@ -19,5 +17,4 @@ function copyEnvVarsToGradleProperties {
         echo "artifactory_password=(cat $BUILDKITE_HOME/.artifactory-password)" >> $GRADLE_PROPERTIES
         echo "artifactory_contextURL=$artifactory_contextURL" >> $GRADLE_PROPERTIES
         echo "artifactory_repo=android-libs-dev" >> $GRADLE_PROPERTIES
-    fi
-}
+
