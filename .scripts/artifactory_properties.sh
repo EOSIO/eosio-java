8a39b5fd3f1ef19684e9202d166937fff0656bc5
Shawn Edge <shawn.edge@block.one>
2019-04-24 14:16:43 -0400

    change url format

--- .scripts/artifactory_properties.sh
+++ .scripts/artifactory_properties.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+artifactory_contextURL="https://blockone.jfrog.io/blockone"
+
 function copyEnvVarsToGradleProperties {
     GRADLE_PROPERTIES=$BUILDKITE_HOME"/EOSIO/eosio-java/eosiojava/gradle.properties"
     export GRADLE_PROPERTIES
@@ -15,7 +17,7 @@ function copyEnvVarsToGradleProperties {
         echo "Writing Secrets to gradle.properties..."
         echo "artifactory_username=(cat $BUILDKITE_HOME/.artifactory-username)" >> $GRADLE_PROPERTIES
         echo "artifactory_password=(cat $BUILDKITE_HOME/.artifactory-password)" >> $GRADLE_PROPERTIES
-        echo "artifactory_contextURL=(https://blockone.jfrog.io/blockone)" >> $GRADLE_PROPERTIES
+        echo "artifactory_contextURL=$artifactory_contextURL" >> $GRADLE_PROPERTIES
         echo "artifactory_repo=android-libs-dev" >> $GRADLE_PROPERTIES
     fi
 }
