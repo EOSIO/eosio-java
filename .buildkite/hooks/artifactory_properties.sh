35d431755b3d5e7b58f5990e7d921dbba90404f4
Shawn Edge <shawn.edge@block.one>
2019-04-24 15:30:41 -0400

    echo url

--- .buildkite/hooks/artifactory_properties.sh
+++ .buildkite/hooks/artifactory_properties.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-artifactory_contextURL="https://blockone.jfrog.io/blockone"
+ export artifactory_contextURL="https://blockone.jfrog.io/blockone"
 
     GRADLE_PROPERTIES="$BUILDKITE_HOME/EOSIO/eosio-java/eosiojava/gradle.properties"
     export GRADLE_PROPERTIES
@@ -16,5 +16,5 @@ artifactory_contextURL="https://blockone.jfrog.io/blockone"
         echo "artifactory_username=(cat $BUILDKITE_HOME/.artifactory-username)" >> $GRADLE_PROPERTIES
         echo "artifactory_password=(cat $BUILDKITE_HOME/.artifactory-password)" >> $GRADLE_PROPERTIES
         echo "artifactory_contextURL=$artifactory_contextURL" >> $GRADLE_PROPERTIES
-        echo "artifactory_repo=android-libs-dev" >> $GRADLE_PROPERTIES
+        echo "$artifactory_contextURL" >> $GRADLE_PROPERTIES
 
