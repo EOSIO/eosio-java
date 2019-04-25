dc3b616ca09ec3d06867e3ec6027c3990a80b30c
Shawn Edge <shawn.edge@block.one>
2019-04-25 09:29:02 -0400

    grand slam 4

--- .scripts/artifactory.sh
+++ .scripts/artifactory.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
 
-export artifactory_contextURL="https://blockone.jfrog.io/blockone/webapp/#/login"
 GRADLE_PROPERTIES="/var/lib/buildkite-agent/builds/automation-android-builder-fleet/EOSIO/eosio-java/eosiojava/gradle.properties"
 export GRADLE_PROPERTIES
 echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
@@ -11,6 +10,6 @@ touch $GRADLE_PROPERTIES
 echo "Writing Secrets to gradle.properties..."
 echo "artifactory_username=$(cat /var/lib/buildkite-agent/.artifactory-username)" >> $GRADLE_PROPERTIES
 echo "artifactory_password=$(cat /var/lib/buildkite-agent/.artifactory-password)" >> $GRADLE_PROPERTIES
-echo "artifactory_contextURL=https://blockone.jfrog.io/blockone" >> $GRADLE_PROPERTIES
+echo "artifactory_contextURL=https://blockone.jfrog.io/blockone/webapp/#/login" >> $GRADLE_PROPERTIES
 echo "artifactory_repo=android-libs-dev" >> $GRADLE_PROPERTIES
 
