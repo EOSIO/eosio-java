ceb0d64487afdbbb7be71bb636f28191fa35b38e
Shawn Edge <shawn.edge@block.one>
2019-04-24 22:37:28 -0400

    script echo

--- .scripts/artifactory.sh
+++ .scripts/artifactory.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
+export username_test=(cat ~/var/lib/buildkite-agent/.artifactory-username)
 export artifactory_contextURL="https://blockone.jfrog.io/blockone"
 GRADLE_PROPERTIES="/var/lib/buildkite-agent/builds/automation-android-builder-fleet/EOSIO/eosio-java/eosiojava/gradle.properties"
 export GRADLE_PROPERTIES
@@ -10,6 +11,7 @@ echo "Creating Gradle Properties file..."
 touch $GRADLE_PROPERTIES
 echo "Writing Secrets to gradle.properties..."
 echo "artifactory_username=(cat ~/var/lib/buildkite-agent/.artifactory-username)" >> $GRADLE_PROPERTIES
+echo $username_test
 echo "artifactory_password=(cat ~/var/lib/buildkite-agent/.artifactory-password)" >> $GRADLE_PROPERTIES
 echo "artifactory_contextURL=$artifactory_contextURL" >> $GRADLE_PROPERTIES
 echo "$artifactory_contextURL" >> $GRADLE_PROPERTIES
