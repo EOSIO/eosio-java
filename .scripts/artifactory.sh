c9a0f1bb9d09ff569e7d59218bbbff553484065e
Shawn Edge <shawn.edge@block.one>
2019-04-24 22:06:51 -0400

    script echo

--- .scripts/artifactory.sh
+++ .scripts/artifactory.sh
@@ -2,12 +2,13 @@
 set -eu -o pipefail
 
 export artifactory_contextURL="https://blockone.jfrog.io/blockone"
+gradle test || fail
 GRADLE_PROPERTIES="/home/centos/.gradle/gradle.properties"
 export GRADLE_PROPERTIES
 echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
 echo "Gradle Properties does not exist"
 echo "Creating Gradle Properties file..."
-sudo touch $GRADLE_PROPERTIES
+touch $GRADLE_PROPERTIES
 echo "Writing Secrets to gradle.properties..."
 echo "artifactory_username=(cat $BUILDKITE_HOME/.artifactory-username)" >> $GRADLE_PROPERTIES
 echo "artifactory_password=(cat $BUILDKITE_HOME/.artifactory-password)" >> $GRADLE_PROPERTIES
