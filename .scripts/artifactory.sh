7b2590bf95e13b6ad674197444da60b6d13df5c9
Shawn Edge <shawn.edge@block.one>
2019-04-24 22:18:50 -0400

    script echo

--- .scripts/artifactory.sh
+++ .scripts/artifactory.sh
@@ -9,8 +9,8 @@ echo "Gradle Properties does not exist"
 echo "Creating Gradle Properties file..."
 touch $GRADLE_PROPERTIES
 echo "Writing Secrets to gradle.properties..."
-echo "artifactory_username=(cat $BUILDKITE_HOME/.artifactory-username)" >> $GRADLE_PROPERTIES
-echo "artifactory_password=(cat $BUILDKITE_HOME/.artifactory-password)" >> $GRADLE_PROPERTIES
+echo "artifactory_username=(cat ~/.artifactory-username)" >> $GRADLE_PROPERTIES
+echo "artifactory_password=(cat ~/.artifactory-password)" >> $GRADLE_PROPERTIES
 echo "artifactory_contextURL=$artifactory_contextURL" >> $GRADLE_PROPERTIES
 echo "$artifactory_contextURL" >> $GRADLE_PROPERTIES
 
