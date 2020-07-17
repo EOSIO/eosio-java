f9cf1cdd72407fafc911285f7a80b0357aa7b87d
Jon La Marr <jon.lamarr-ext@block.one>
2020-07-17 19:56:41 -0400

    Add constants

---
+++ eosiojava/src/main/java/one/block/eosiojava/models/queryit/QueryItConstants.java
@@ -0,0 +1,25 @@
+package one.block.eosiojava.models.queryit;
+
+public class QueryItConstants {
+    public static final String ANY_OBJECT_TYPE = "any_object";
+    public static final String ANY_ARRAY_TYPE = "any_array";
+    public static final String NULL_T_TYPE = "null_t";
+    public static final String INT64_TYPE = "int64";
+    public static final String UINT64_TYPE = "uint64";
+    public static final String INT32_TYPE = "int32";
+    public static final String UINT32_TYPE = "uint32";
+    public static final String INT16_TYPE = "int16";
+    public static final String UINT16_TYPE = "uint16";
+    public static final String INT8_TYPE = "int8";
+    public static final String UINT8_TYPE = "uint8";
+    public static final String TIME_POINT_TYPE = "time_point";
+    public static final String CHECKSUM256_TYPE = "checksum256";
+    public static final String FLOAT64_TYPE = "float64";
+    public static final String STRING_TYPE = "string";
+    public static final String BYTES_TYPE = "bytes";
+    public static final String SYMBOL_TYPE = "symbol";
+    public static final String SYMBOL_CODE_TYPE = "symbol_code";
+    public static final String ASSET_TYPE = "asset";
+
+    public static final char OPTIONAL_ZONE_CHAR = 'Z';
+}
--- eosiojava/src/main/java/one/block/eosiojava/models/queryit/QueryItDeserializer.java
+++ eosiojava/src/main/java/one/block/eosiojava/models/queryit/QueryItDeserializer.java
@@ -4,18 +4,12 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
 import java.lang.reflect.Type;
 import java.math.BigInteger;
 import java.time.Instant;
-import java.util.List;
 
 public class QueryItDeserializer implements JsonDeserializer<QueryIt> {
-    private static final String ANY_OBJECT_TYPE = "any_object";
-    private static final String ANY_ARRAY_TYPE = "any_array";
-
     @Override
     public QueryIt deserialize(JsonElement json, Type typeOfT,
             JsonDeserializationContext context) throws JsonParseException {
@@ -27,45 +21,33 @@ public class QueryItDeserializer implements JsonDeserializer<QueryIt> {
             JsonArray jsonAsArray = ((JsonArray) json);
             String type = jsonAsArray.get(0).getAsString();
 
-            if (type.equals("int32")
-                    || type.equals("int16")
-                    || type.equals("uint16")
-                    || type.equals("int8")
-                    || type.equals("uint8")) {
+            if (type.equals(QueryItConstants.INT32_TYPE)
+                    || type.equals(QueryItConstants.INT16_TYPE) || type.equals(QueryItConstants.UINT16_TYPE)
+                    || type.equals(QueryItConstants.INT8_TYPE) || type.equals(QueryItConstants.UINT8_TYPE)) {
                 queryIt.setValue(Integer.parseInt(jsonAsArray.get(1).getAsString()));
-            } else if (type.equals("uint64")) {
+            } else if (type.equals(QueryItConstants.UINT64_TYPE)) {
                 queryIt.setValue(new BigInteger(jsonAsArray.get(1).getAsString()));
-            } else if (type.equals("float64")) {
+            } else if (type.equals(QueryItConstants.FLOAT64_TYPE)) {
                 queryIt.setValue(Float.parseFloat(jsonAsArray.get(1).getAsString()));
-            } else if (type.equals("int64") || type.equals("uint32")) {
+            } else if (type.equals(QueryItConstants.INT64_TYPE) || type.equals(QueryItConstants.UINT32_TYPE)) {
                 queryIt.setValue(Long.parseLong(jsonAsArray.get(1).getAsString()));
-            } else if (type.equals("time_point")) {
+            } else if (type.equals(QueryItConstants.TIME_POINT_TYPE)) {
                 String timePointAsString = jsonAsArray.get(1).getAsString();
-                if (timePointAsString.charAt(timePointAsString.length() - 1) != 'Z') {
-                    timePointAsString += 'Z';
+                if (timePointAsString.charAt(timePointAsString.length() - 1) != QueryItConstants.OPTIONAL_ZONE_CHAR) {
+                    timePointAsString += QueryItConstants.OPTIONAL_ZONE_CHAR;
                 }
                 queryIt.setValue(Instant.parse(timePointAsString));
-            } else if (type.equals("any_object")) {
+            } else if (type.equals(QueryItConstants.ANY_OBJECT_TYPE)) {
                 JsonArray array = jsonAsArray.get(1).getAsJsonArray();
                 for(JsonElement subJson : array) {
-//                    JsonObject subJsonAsObject = subJson.getAsJsonObject();
-//                    String name = subJsonAsObject.get("name").getAsString();
-//                    JsonArray subJsonArray = subJsonAsObject.getAsJsonArray("value");
-//                    QueryIt subQueryIt = deserialize(subJsonArray, typeOfT, context);
-//                    subQueryIt.setName(name);
-//                    queryIt.addField(subQueryIt);
                     Field field = context.deserialize(subJson, Field.class);
                     queryIt.addField(field);
                 }
-                //queryIt.value = "test";
-            } else if (type.equals("any_array")) {
+            } else if (type.equals(QueryItConstants.ANY_ARRAY_TYPE)) {
                 JsonArray array = jsonAsArray.get(1).getAsJsonArray();
                 for(JsonElement subJson : array) {
                     QueryIt subQueryIt = deserialize(subJson, typeOfT, context);
                     queryIt.addQueryIt(subQueryIt);
-//                    JsonArray nestedArray = subJson.getAsJsonArray().get(1).getAsJsonArray();
-//                    Field field = context.deserialize(nestedArray, Field.class);
-//                    queryIt.addField(field);
                 }
             } else { // string, checksum256, symbol, symbol_code, asset, bytes
                 queryIt.setValue(jsonAsArray.get(1).getAsString());
